1d8f3c24ffcbd4ec26a97a8dc7cdfc6060013169
Beth Johnson <bethjohnson3@gmail.com>
2017-03-16 09:23:00 -0700

    Add database

---
+++ database/index.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+
+db.on('error', function() {
+  console.log('mongoose connection error');
+});
+
+db.once('open', function() {
+  console.log('mongoose connected successfully');
+});
+
+var pizzaSchema = mongoose.Schema({
+  quantity: Number,
+  sauce: String,
+  topping1: String,
+  topping2: String,
+  topping3: String
+});
+
+var Pizza = mongoose.model('Pizza', pizzaSchema);
+
+var selectAll = function(callback) {
+  Pizza.find({}, function(err, items) {
+    if(err) {
+      callback(err, null);
+    } else {
+      callback(null, items);
+    }
+  });
+};
+
+module.exports.selectAll = selectAll;
